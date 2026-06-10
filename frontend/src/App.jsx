fcf24fe681639d7ef38a16ac8d54b4450620396b
Nabin Singh <nabin@skilluplabs.com.au>
2026-06-10 20:35:37 +1000

    As a user I should be able to add new task and see it as soon as I add it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import './App.css'
+import ToDoForm from './components/ToDoForm';
 
 function App() {
-  const todoTasks = [
+  const [todoTasks, setTodoTasks] = useState([
     'Go to the gym',
     'Buy groceries',
     'Finish the project',
     'Call mom',
     'Read a book',
     'Plan the weekend trip',
-  ];
+  ]);
+
+  function handleAddTask(newTask) {
+    console.log("New task added:", newTask);
+    setTodoTasks([newTask, ...todoTasks]);
+  }
 
   return (
     <>
       <h2>Todo App  </h2>
+
+      <ToDoForm handleAddTask={handleAddTask} />
 
       <ul>
         {
